ecef0be6fc8123924d1226c5ede14363339d4e68
Christian <christian@christian-R480-R431-R481.(none)>
2014-03-12 23:04:29 -0600

    Unos pequeños cambios

--- Practica/Practica/settings.py
+++ Practica/Practica/settings.py
@@ -1,33 +1,14 @@
-"""
-Django settings for Practica project.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.6/topics/settings/
-
-For the full list of settings and their values, see
-https://docs.djangoproject.com/en/1.6/ref/settings/
-"""
-
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'uu*a(ah%971+67wf$v*+=7$#63wrn#gtzl7)6r0%m2)^8$a580'
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 TEMPLATE_DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
-# Application definition
 
 INSTALLED_APPS = (
     'django.contrib.admin',
@@ -36,6 +17,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Principal',
+    'south'
 )
 
 MIDDLEWARE_CLASSES = (
@@ -52,18 +35,17 @@ ROOT_URLCONF = 'Practica.urls'
 WSGI_APPLICATION = 'Practica.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'mydb.db',                      # Or path to database file if using sqlite3.
+        # The following settings are not used with sqlite3:
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': '',                      # Set to empty string for default.
     }
 }
-
-# Internationalization
-# https://docs.djangoproject.com/en/1.6/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
 
@@ -75,8 +57,5 @@ USE_L10N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
--- Practica/Principal/models.py
+++ Practica/Principal/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Estudiante(models.Model):
+	username = models.CharField(max_length=140, unique=True)
+	nombre = models.CharField(max_length=140)
+	apellidos = models.CharField(max_length=140)
+	bio = models.TextField(null=True)
+	fecha_nac = models.DateField(blank = True, null = True)
+	email = models.EmailField(blank=True, null=True)
